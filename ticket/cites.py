378f82ff6a0f257a9ce3a083c9347252e8e432cc
林冲 <729883852@qq.com>
2018-01-10 23:44:45 +0800

    12306抢票

---
+++ ticket/cites.py
@@ -0,0 +1,15 @@
+# 车次信息，从cookie中取得,如需要自行去12306
+cities = {'成都': '%u6210%u90FD%2CCDW',
+          '重庆': '%u91CD%u5E86%2CCQW',
+          '北京': '%u5317%u4EAC%2CBJP',
+          '广州': '%u5E7F%u5DDE%2CGZQ',
+          '杭州': '%u676D%u5DDE%2CHZH',
+          '宜昌': '%u5B9C%u660C%2CYCN',
+          '郑州': '%u90D1%u5DDE%2CZZF',
+          '深圳': '%u6DF1%u5733%2CSZQ',
+          '西安': '%u897F%u5B89%2CXAY',
+          '大连': '%u5927%u8FDE%2CDLT',
+          '武汉': '%u6B66%u6C49%2CWHN',
+          '上海': '%u4E0A%u6D77%2CSHH',
+          '南京': '%u5357%u4EAC%2CNJH',
+          '合肥': '%u5408%u80A5%2CHFH'}
